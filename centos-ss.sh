4ebcd63007e9300a1da79f29b4814620e4fe2350
baiguiren <baiguiren@kollway.com>
2016-12-15 10:45:08 +0800

    remove sudo

--- centos-ss.sh
+++ centos-ss.sh
@@ -1,4 +1,4 @@
-sudo yum install m2crypto python-setuptools
-sudo easy_install pip
-sudo pip install shadowsocks
+ yum install m2crypto python-setuptools
+ easy_install pip
+ pip install shadowsocks
 
