1007c8ba5b9e3d1554afc7d8bcfdda2d21d295cc
baiguiren <baiguiren@kollway.com>
2016-12-15 12:50:28 +0800

    modify port

--- centos-ss.sh
+++ centos-ss.sh
@@ -1,5 +1,5 @@
- yum install m2crypto python-setuptools
- easy_install pip
- pip install shadowsocks
+yum install m2crypto python-setuptools
+easy_install pip
+pip install shadowsocks
 
 cp shadowsocks.json /etc/shadowsocks.json
