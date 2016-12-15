91366bd0f53c04f86511c44afaf9ac2f2bd72658
baiguiren <baiguiren@kollway.com>
2016-12-15 10:47:56 +0800

    add config

--- centos-ss.sh
+++ centos-ss.sh
@@ -2,3 +2,16 @@
  easy_install pip
  pip install shadowsocks
 
+config=$(cat <<< EOF
+{
+"server":"your_server_ip",
+"server_port":8000,
+"local_port":1080,
+"password":"your_passwd",
+"timeout":600,
+"method":"aes-256-cfb"
+}
+EOF
+)
+
+echo ${config} > /etc/shadowsocks.json
